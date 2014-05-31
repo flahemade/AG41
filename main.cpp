3457405067e2bd59e5e481983521636383d9198d
shadowsky <sbdjamel@hotmail.com>
2014-06-01 01:15:41 +0200

    new fichier

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  AG41
+//
+//  Created by djamel on 01/06/2014.
+//  Copyright (c) 2014 djamel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	//fonction tri par date
+	
+	//fonction qui grouperai les clients
+    return 0;
+}
+
